0d540d74d46b86a330c9593d5b4f5d48037162c4
Samuel Dindyal <sam.dindyal@icloud.com>
2016-04-04 18:30:18 -0400

    Adds shell for global count

--- lib/a3.ex
+++ lib/a3.ex
@@ -1,7 +1,6 @@
 defmodule Assignment3 do
 
   @tagCount %{}
-  @counter 0
   @links %{}
 
   def run(url, pages, maxDepth, currentDepth) do
@@ -14,15 +13,25 @@ defmodule Assignment3 do
 
     if tags != nil && currentDepth < maxDepth - 1 do
       [links, tagCount] = WebStats.parseHTML(tags, %{}, %{})
-      cascadeTagCounts(tagCount)
+      @tagCount = cascadeTagCounts(tagCount)
       followLoop(Map.keys(links), pages, maxDepth, currentDepth)
+    end
+
+    if (currentDepth == 0) do
+      IO.puts "--------------------------------"
+      IO.puts "GLOBAL COUNT"
+      IO.puts "ROOT URL: #{url}\nMaxPages: #{pages}\t MaxDepth: #{maxDepth}\tCurrentDepth: #{currentDepth}"
+      IO.puts "--------------------------------"
+
+      for tag <- Map.keys(@tagCount) do
+        IO.puts "#{tag}"
+      end
     end
   end
 
   def followLoop([], _, _, _) do end
 
   def followLoop(_, 0, _, _) do end
-
 
   def followLoop([link | link_tail], maxFollow, maxDepth, currentDepth) do
     run(link, maxFollow, maxDepth, currentDepth+1)
@@ -34,16 +43,26 @@ defmodule Assignment3 do
       pages = args[:maxPages] || 10
       depth = args[:maxDepth] || 3
       run(url, pages, depth, 0)
+      @tagCount
   end
 
   def cascadeTagCounts(tagCount) do
       for tag <- Map.keys(tagCount) do
         if Map.has_key?(@tagCount, tag) do
-            @tagCount = %{ @tagCount | tag => tagCount[tag] + @tagCount[tag] }
+          # IO.puts "######\nYES #{tag} = #{globalTagCount[tag]}\n######"
+            # @tagCount = %{ @tagCount | tag => (tagCount[tag] + @tagCount[tag]) }
+            # IO.puts tagCount[tag] + @tagCount[tag]
+            # IO.puts "#{tag} EXISTS @tagcount[#{tag}] #{@tagCount[tag]}"
+            @tagCount = Map.put(tagCount, tag, tagCount[tag] + @tagCount[tag])
+            # IO.puts "#{globalTagCount[tag]}"
         else
-            @tagCount = Map.put(@tagCount, tag, 1)
+          # IO.puts "######\nNO #{tag} = #{globalTagCount[tag]}\n######"
+          # globalTagCount = %{ globalTagCount | tag => 1 }
+          # IO.puts "#{tag} DOES NOT EXIST}"
+          @tagCount = Map.put(tagCount, tag, 1)
         end
       end
+      @tagCount
   end
 
   def main(args) do end
