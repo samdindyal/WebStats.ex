6209fbcded5d46101f43f24ea6fe5a066c3c1c6a
Samuel Dindyal <sam.dindyal@icloud.com>
2016-04-08 16:54:43 -0400

    Bug fix

--- lib/a3.ex
+++ lib/a3.ex
@@ -1,17 +1,17 @@
-@moduledoc """
--------------------------------------------------------------
-Name: Sam Dindyal, Beverly Li
-Course: CPS506, Winter 2016, Assignment 3
-Due: April 5th 2016
-Credit: This is entirely my own work.
-
-This project is a simulation of a web crawler written in Elixir. Further details about our assignment can be found on GitHub at 	https://github.com/samdindyal/WebStats-Elixir. To efficiently identify all the start-tags and links on a web page, our crawler uses regular expressions and stores the matches in two seperate hash maps. The first hash map consists of start-tags. This hash map is used to keep track of the type of start-tag found and the count of that tag. The second hash map stores the links, and consists of the string value of the link and its visit status. All links start off with the visit status set to false. As each page is visited, the status of the link is updated in the hash map to true.
-
-In the best case scenario, all links found on the web page would be non-relative (in which case the crawler will follow it). To maximize the use of cores, the crawler uses concurrent threads. Every link followed by the crawler is assigned a thread. Agents in Elixir are used to allow threads to retrieve and update the hash maps of the start-tags and links. Each time a web page is done scanning, the thread updates the global count of the start-tags by merging the hash map of the current webpage to the global hash map of the start-tags.
--------------------------------------------------------------
-"""
-
 defmodule Assignment3 do
+
+  @moduledoc """
+  -------------------------------------------------------------
+  Name: Sam Dindyal, Beverly Li
+  Course: CPS506, Winter 2016, Assignment 3
+  Due: April 5th 2016
+  Credit: This is entirely my own work.
+
+  This project is a simulation of a web crawler written in Elixir. Further details about our assignment can be found on GitHub at 	https://github.com/samdindyal/WebStats-Elixir. To efficiently identify all the start-tags and links on a web page, our crawler uses regular expressions and stores the matches in two seperate hash maps. The first hash map consists of start-tags. This hash map is used to keep track of the type of start-tag found and the count of that tag. The second hash map stores the links, and consists of the string value of the link and its visit status. All links start off with the visit status set to false. As each page is visited, the status of the link is updated in the hash map to true.
+
+  In the best case scenario, all links found on the web page would be non-relative (in which case the crawler will follow it). To maximize the use of cores, the crawler uses concurrent threads. Every link followed by the crawler is assigned a thread. Agents in Elixir are used to allow threads to retrieve and update the hash maps of the start-tags and links. Each time a web page is done scanning, the thread updates the global count of the start-tags by merging the hash map of the current webpage to the global hash map of the start-tags.
+  -------------------------------------------------------------
+  """
 
   def run(url, pages, maxDepth, currentDepth) do
     HTTPoison.start
@@ -60,8 +60,8 @@ defmodule Assignment3 do
   end
 
   def startOn(url, args \\ []) do
-      pages = args[:maxPages] || 10
-      depth = args[:maxDepth] || 3
+      pages = args[:maxPages] || 3
+      depth = args[:maxDepth] || 10
       run(url, pages, depth, 0)
   end
 
@@ -79,29 +79,6 @@ defmodule Assignment3 do
 
   def mergeLinks([link|links], map1, map2) do
     mergeLinks(links, Map.put(map1, link, map1[link] || map2[link]), map2)
-  end
-
-  def start_server do
-    Task.start_link(fn -> handler end)
-  end
-
-  def handler do
-    processes = []
-
-    receive do
-      {:done, link, process, tagCount} ->
-        processes = List.delete(processes, process)
-
-        tagCounts = Agent.get(tagA)
-
-        if List.first(processes) == nil do
-          IO.puts "--------------------------------"
-          IO.puts "GLOBAL COUNT"
-          IO.puts "ROOT URL: #{url}\nMaxPages: #{pages}\t MaxDepth: #{maxDepth}\tCurrentDepth: #{currentDepth}"
-          IO.puts "--------------------------------"
-          WebStats.printTagCount(tagCounts)
-          Process.exit(self(), "Done.")
-        end
   end
 
   def main(args) do
